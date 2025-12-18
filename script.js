022f1013c68710289d759adee28694cd3c8f86d8
Hari-2612 <dharaniharini146@gmail.com>
2025-12-18 14:38:56 +0530

    resolve merge conflict

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 // Simple welcome message
 window.onload = () => {
   console.log("Welcome to Harini's Portfolio 🚀");
 };
-=======
 // Simple welcome message
 window.onload = () => {
   console.log("Welcome to Harini's Portfolio 🚀");
 };
->>>>>>> a7b93ebafc148f81858968192ddafe6f527e667e
