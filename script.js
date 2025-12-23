fb8ee1bb15470e593265169e79aa8e67bf32c1f1
Hari-2612 <dharaniharini146@gmail.com>
2025-12-23 22:49:48 +0530

    update files

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// Simple welcome message
-window.onload = () => {
-  console.log("Welcome to Harini's Portfolio 🚀");
-};
-// Simple welcome message
-window.onload = () => {
-  console.log("Welcome to Harini's Portfolio 🚀");
-};
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+      alert("Navigating to " + this.getAttribute('href').substring(1) + " section!");
+      alert("Enjoy the smooth scroll!");
+      alert("Have a great day!");
+  });
+});
+
